90b78c9620b4d1fbd7500514deeb59bda371a8fa
Evandro Henrique Castro <evancastro31@hotmail.com>
2024-07-31 11:51:33 -0300

    Tipo String

--- Portfolio/src/fundamentos/Import.java
+++ Portfolio/src/fundamentos/Import.java
@@ -1,9 +1,20 @@
 package fundamentos;
 
+import java.util.Date;//necessario para data
+
+import javax.swing.JButton;//necessario para utilizaçao do botao
+
 public class Import {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Date d  = new Date();
+		
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+		
 
 	}
 
