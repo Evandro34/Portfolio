db121b7f5597e737893447974fc94ea8035f31da
Evandro Henrique Castro <evancastro31@hotmail.com>
2024-07-31 08:53:38 -0300

    imprimindo a mensagem Hello Word

---
+++ Portfolio/src/view/HelloWord.java
@@ -0,0 +1,13 @@
+package view;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// impressão da mensagem
+		System.out.println("Hello Word");
+
+	}
+
+}
