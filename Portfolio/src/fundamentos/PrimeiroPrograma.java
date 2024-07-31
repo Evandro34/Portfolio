0c7957a70a84d91a9f4012717308a0c9543dcc2a
Evandro Henrique Castro <evancastro31@hotmail.com>
2024-07-31 10:05:03 -0300

    variavel e constates

--- Portfolio/src/fundamentos/PrimeiroPrograma.java
+++ Portfolio/src/fundamentos/PrimeiroPrograma.java
@@ -2,9 +2,23 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 	
+	/**
+	 * 
+	 * @author EHC
+	 * 
+	 */
+ 
 	public static void main(String[] args) {
 		
-		System.out.println("Primeiro Programa");
+		
+		/*linha 1
+		 * linha 2
+		 * linha 3
+		 */
+		
+		
+
+		System.out.println("Primeiro Programa!!!");//imprime uma mensagem no console;
 	}
 
 }
