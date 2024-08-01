eef0e0289686a1ce4c5c40c0da3eabe51a5436da
Evandro Henrique Castro <evancastro31@hotmail.com>
2024-08-01 20:17:51 -0300

    Operadores logicos

---
+++ Portfolio/src/fundamentos/Logica.java
@@ -0,0 +1,45 @@
+package fundamentos;
+
+public class Logica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
+
+		System.out.println(condicao1 && !condicao2);
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2);
+		System.out.println(!!condicao1);
+		System.out.println(!condicao2);
+
+		System.out.println("tabela verdade E");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		System.out.println(false && false);
+
+		System.out.println("tabela verdade OR");
+		System.out.println(true || true);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+
+		
+
+	 
+		System.out.println("tabela verdade XOR");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+		
+		
+		System.out.println("tabela verdade NOT");
+		System.out.println(!true );
+		System.out.println(!false);
+		
+	}
+
+}
