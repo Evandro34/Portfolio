ad5979ba4e132f5df19bcc3a540143708cc71ba4
Evandro Henrique Castro <evancastro31@hotmail.com>
2024-08-01 20:32:05 -0300

    Desafio Logico

---
+++ Portfolio/src/Desafio/DesafioLogicos.java
@@ -0,0 +1,28 @@
+package Desafio;
+
+public class DesafioLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean trabalho1 = false;
+		boolean trabalho2 = false;
+		
+		
+		
+		boolean comprouTv50 = trabalho1 && trabalho2;
+		boolean comproutv32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		boolean maisSaudavel = !comprouSorvete;
+		
+		System.out.println("Comprou tv 50\"?" + comprouTv50);
+		
+		System.out.println("Comprou tv 32\"?" + comproutv32);
+		
+		System.out.println("Comprou Sorvete ?" + comprouSorvete);
+		
+		System.out.println("Mais saudável?"+ maisSaudavel);
+
+	}
+
+}
