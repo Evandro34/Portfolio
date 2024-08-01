277a64c6affe26f05e3b7b86dcfda60b101c9a46
Evandro Henrique Castro <evancastro31@hotmail.com>
2024-08-01 20:54:14 -0300

    Unarios

--- Portfolio/src/fundamentos/Atribuicao.java
+++ Portfolio/src/fundamentos/Atribuicao.java
@@ -5,6 +5,18 @@ public class Atribuicao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 3;
+		int b = a;
+		int c = a + b;
+
+		c = c + b;
+		c += b;
+		c *= b;
+		c -= b;
+		c /= b;
+		c %= 2;
+		System.out.println(c);
+
 	}
 
 }
